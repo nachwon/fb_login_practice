72dd0f0671ba02f9504a2f37d15aa539b120355f
Che1 <nachwon@gmail.com>
2017-11-13 23:14:24 +0900

    facebook login button

--- fb_login/config/urls.py
+++ fb_login/config/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from login.views import index, login
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name='index'),
+    url(r'^fb-login/$', login, name='login')
 ]
--- fb_login/login/views.py
+++ fb_login/login/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    app_id = 1552973814793221
+    context = {
+        "app_id": app_id,
+    }
+    return render(request, 'login.html', context)
+
+
+def login(request):
+    return HttpResponse('hello')
