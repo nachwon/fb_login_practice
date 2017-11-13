d6b079f767e908bfb33bbe57fdc2935b831a4608
Che1 <nachwon@gmail.com>
2017-11-14 00:29:41 +0900

    access token

--- fb_login/login/views.py
+++ fb_login/login/views.py
@@ -1,9 +1,13 @@
+import requests
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
+
+app_id = 1552973814793221
+app_secret = 'bb9f21234cde9c81be4df7c13f6f82b9'
 
 
 def index(request):
-    app_id = 1552973814793221
     context = {
         "app_id": app_id,
     }
@@ -11,4 +15,22 @@ def index(request):
 
 
 def login(request):
-    return HttpResponse('hello')
+    code = request.GET['code']
+    redirect_uri = f"{request.scheme}://{request.META['HTTP_HOST']}{reverse('login')}"
+    url_access_token = "https://graph.facebook.com/v2.11/oauth/access_token?" \
+                       "client_id={app-id}" \
+                       "&redirect_uri={redirect-uri}" \
+                       "&client_secret={app-secret}" \
+                       "&code={code-parameter}"
+
+    params_access_token = {
+        "client_id": app_id,
+        "redirect_uri": redirect_uri,
+        "client_secret": app_secret,
+        "code": code,
+    }
+
+    response = requests.get(url_access_token, params=params_access_token)
+    result = response.json()
+
+    return HttpResponse(result)
